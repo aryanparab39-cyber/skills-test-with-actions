eb92798e761cb80440cfc8457aecf5e7dcec5558
aryanparab39-cyber <aryanparab39@gmail.com>
2026-07-30 18:22:36 +0000

    Fix Fibonacci implementation and increase test coverage

--- src/calculations.py
+++ src/calculations.py
@@ -1,8 +1,4 @@
-# System Modules
 import math
-
-# Installed Modules
-# - None
 
 
 def area_of_circle(radius):
@@ -22,6 +18,6 @@ def get_nth_fibonacci(n):
         return 1
     else:
         a, b = 0, 1
-        for _ in range(2, n + 1):
+        for _ in range(1, n + 1):
             a, b = b, a + b
         return b
